1ee5af71617e75f31a4f4f6d908ce5d0e89f1100
Christian Norrman <christian@faxity.se>
2019-08-31 22:28:47 +0200

    Added support for native recursive mkdir

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-const { promisify } = require('util');
-
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+
 const stat = promisify(fs.stat);
 const mkdir = promisify(fs.mkdir);
+const version = process.version.substring(1).split('.').map(i => i | 0);
 
 module.exports = async function mkdirp(dirpath, mode = 0777) {
   dirpath = path.resolve(dirpath);
 
+  // Use native mkdirp function if supported (v10.12.0)
+  if (version[0] > 10 || (version[0] == 10 && version[1] >= 12)) {
+    return mkdir(dirpath, { recursive: true, mode });
+  }
+
   try {
     await mkdir(dirpath, mode);
     return true;
-  }
-  catch(ex) {
-    // If parent dir doesnt exist then create parent directory first then try again
+  } catch(ex) {
     if(ex.code == 'ENOENT') {
+      // Create parent directory then try again
       await mkdirp(path.dirname(dirpath), mode);
       return await mkdirp(dirpath, mode);
     } else {
-      // In the case of any other error, just see if there's a dir there already.
       try {
-        // Check if something else is borker or if directory already exists
+        // If not a directory then throw error, if not then just return false to indicate we didn't create a directory.
         const dirstat = await stat(dirpath);
+
         if(!dirstat.isDirectory()) throw ex;
 
-        // Directory exists already yay! report that none was created
         return false;
-      } catch(_) {
-        // If stat fails then throw first exception it's for weirdness
+      } catch (_) {
+        // If stat fails then throw first error
         throw ex;
       }
     }
